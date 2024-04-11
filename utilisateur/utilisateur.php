ef9570ec9ac7b850cbcba3685820993a4cecc3e3
Mathis-PCtour <twitch.mathis.boschian@gmail.com>
2024-04-11 18:09:27 +0200

    deconnexion fait !

--- utilisateur/utilisateur.php
+++ utilisateur/utilisateur.php
@@ -74,15 +74,9 @@
                 if (session_status() == PHP_SESSION_NONE) {
                     session_start();
                 }
-
-                // Unset all of the session variables
                 $_SESSION = array();
-
-                // Destroy the session.
                 session_destroy();
-
-                // Redirect to login page
-                header('Location: login.php');
+                header('Location: ../index.php');
                 exit;
             }
         ?>

8181e7547a3698960c0fe9121189a0a2b2d1a56f
Mathis-PCtour <twitch.mathis.boschian@gmail.com>
2024-05-01 12:51:26 +0200

    fonction pour recup les bonne pratique et affichage quand on click mais bug pour le moment

--- utilisateur/utilisateur.php
+++ utilisateur/utilisateur.php
@@ -81,13 +81,9 @@
         <div class="BP">
         <?php
             $sql = "SELECT 
-                        Appartenance.num_bp,
                         BonnesPratique.test_bp,
-                        BonnesPratique.utilisation_bp,
                         programme.nom_prog,
-                        phase.nom_phase,
-                        Description.num_description,
-                        Motcles.num_cles
+                        phase.nom_phase
                     FROM 
                         Appartenance
                     JOIN 
@@ -98,39 +94,80 @@
                         phase ON Appartenance.num_phase = phase.num_phase
                     JOIN 
                         Description ON Appartenance.num_bp = Description.num_bp
-                    JOIN 
-                        Motcles ON Description.num_cles = Motcles.num_cles
                     LIMIT 0, 25;";
             $stmt = $conn->prepare($sql);
             $stmt->execute();
 
             if ($stmt->rowCount() > 0) {
                 while($row = $stmt->fetch()) {
-                    echo "<div class='bonne-pratique' onclick='openDetails(" . $row["num_bp"] . ")'>";
-                    echo "<h2>" . $row["num_bp"] . "</h2>";
+                    echo "<div class='bonne-pratique'>";
                     echo "<p>Test: " . $row["test_bp"] . "</p>";
-                    echo "<p>Utilisation: " . $row["utilisation_bp"] . "</p>";
                     echo "<p>Programme: " . $row["nom_prog"] . "</p>";
                     echo "<p>Phase: " . $row["nom_phase"] . "</p>";
-                    echo "<p>Description: " . $row["num_description"] . "</p>";
-                    echo "<p>Mot clé: " . $row["num_cles"] . "</p>";
                     echo "</div>";
                 }
             } else {
                 echo "No results found";
             }
         ?>
-
-        <script>
-            function openDetails(num_bp) {
-                var win = window.open('details.php?num_bp=' + num_bp, '_blank', 'width=50%,height=100%');
-                win.focus();
-            }
-        </script>
         </div>
         <div class="valider">
             <button class="button-valider"><H2>valider</H2></button>
         </div>
     </div>
+    <div class="popup">
+            <?php
+                $sql = "SELECT 
+                    Appartenance.num_bp,
+                    BonnesPratique.test_bp,
+                    BonnesPratique.utilisation_bp,
+                    programme.nom_prog,
+                    phase.nom_phase,
+                    Description.num_description,
+                    Motcles.num_cles
+                FROM 
+                    Appartenance
+                JOIN 
+                    BonnesPratique ON Appartenance.num_bp = BonnesPratique.num_bp
+                JOIN 
+                    programme ON Appartenance.num_prog = programme.num_prog
+                JOIN 
+                    phase ON Appartenance.num_phase = phase.num_phase
+                JOIN 
+                    Description ON Appartenance.num_bp = Description.num_bp
+                JOIN 
+                    Motcles ON Description.num_cles = Motcles.num_cles
+                LIMIT 0, 25;";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                if ($stmt->rowCount() > 0) {
+                    while($row = $stmt->fetch()) {
+                        echo "<h2>" . $row["num_bp"] . "</h2>";
+                        echo "<p>Test: " . $row["test_bp"] . "</p>";
+                        echo "<p>Utilisation: " . $row["utilisation_bp"] . "</p>";
+                        echo "<p>Programme: " . $row["nom_prog"] . "</p>";
+                        echo "<p>Phase: " . $row["nom_phase"] . "</p>";
+                        echo "<p>Description: " . $row["num_description"] . "</p>";
+                        echo "<p>Mot clé: " . $row["num_cles"] . "</p>";
+                    }
+                } else {
+                    echo "No results found";
+                }
+            ?>
+            <button class="close-button">
+                Fermer
+            </button>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script>
+        $(".bonne-pratique").on("click", function() {
+            $(".popup").addClass("show");
+        });
+    </script>
+    <script>
+        $(".close-button").on("click", function() {
+            $(".popup").removeClass("show");
+        });
+    </script>
 </body>
 </html>

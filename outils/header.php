dabb5e8f8461ab60ff454427e444c5861842f31a
Djigo Mamoudou <mamoudou.djigo@etu.unice.fr>
2024-05-10 17:22:40 +0200

    ajout mamoudou

--- outils/header.php
+++ outils/header.php
@@ -1,3 +1,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=, initial-scale=1.0">
+<!-- Include JavaScript file for managing user inactivity -->
 <script src="../js/inactivity.js"></script>

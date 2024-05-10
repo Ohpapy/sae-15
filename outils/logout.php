9fb190e133fccf870b0e497c456e74749d855db6
Djigo Mamoudou <mamoudou.djigo@etu.unice.fr>
2024-05-10 17:26:23 +0200

    ajout mamoudou

--- outils/logout.php
+++ outils/logout.php
@@ -1,9 +1,14 @@
 <?php
+    // Check if a session is not already started, then start a session
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
+    // Reset session data by assigning an empty array
     $_SESSION = array();
+    // Destroy the session
     session_destroy();
+    // Redirect user to index.php after session is destroyed
     header('Location: ../index.php');
+    // Ensure no further PHP script is executed after redirection
     exit;
 ?>

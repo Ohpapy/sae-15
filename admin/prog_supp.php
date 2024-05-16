8fa2fdc8e2239ca941790c4c7752c801cab2876f
Mathis-PCtour <twitch.mathis.boschian@gmail.com>
2024-05-16 15:49:28 +0200

    correction pour les programe deja relier a une bonne pratique

--- admin/prog_supp.php
+++ admin/prog_supp.php
@@ -7,6 +7,12 @@
         $stmt->execute();
         $progs = $stmt->fetchAll(PDO::FETCH_ASSOC);
         if (isset($_POST['delete'])) {
+            // Delete the corresponding rows from 'appartenance' first
+            $sqlDeleteAppartenance = "DELETE FROM appartenance WHERE num_prog = ?";
+            $stmtDeleteAppartenance = $conn->prepare($sqlDeleteAppartenance);
+            $stmtDeleteAppartenance->execute([$_POST['num_prog']]);
+
+            // Now delete the row from 'programme'
             $sqlDelete = "DELETE FROM programme WHERE num_prog = ?";
             $stmt = $conn->prepare($sqlDelete);
             $stmt->execute([$_POST['num_prog']]);

600c3618452e783981a539ee148a8bf132e89a69
Mathis-PCtour <twitch.mathis.boschian@gmail.com>
2024-06-08 00:44:22 +0200

    reglage warning dans la console

--- outils/header.php
+++ outils/header.php
@@ -1,4 +1,4 @@
 <meta charset="UTF-8">
-<meta name="viewport" content="width=, initial-scale=1.0">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- Include JavaScript file for managing user inactivity -->
 <script src="../js/inactivity.js"></script>
--- utilisateur/PDF.py
+++ utilisateur/PDF.py
@@ -12,67 +12,47 @@ db = mysql.connector.connect(
     database="rp09"
 )
 
-
+liste_bp = []
 cursor = db.cursor()
 args = sys.argv[1:]
-
-cursor.execute(f"""
-    SELECT DISTINCT appartenance.num_bp, bonnespratique.test_bp, bonnespratique.utilisation_bp, programme.nom_prog, phase.nom_phase
-        FROM appartenance
-        JOIN bonnespratique ON appartenance.num_bp = bonnespratique.num_bp
-        JOIN programme ON appartenance.num_prog = programme.num_prog
-        JOIN phase ON appartenance.num_phase = phase.num_phase
-        JOIN bp_motcles ON appartenance.num_bp = bp_motcles.num_bp
-        JOIN motcles ON bp_motcles.num_cles = motcles.num_cles WHERE 1=1
-    """)
-
-
-data = cursor.fetchall()
-
+for arg in args:
+    cursor.execute(f"""
+            SELECT bonnespratique.num_bp, bonnespratique.test_bp, programme.nom_prog, phase.nom_phase
+            FROM appartenance
+            JOIN bonnespratique ON appartenance.num_bp = bonnespratique.num_bp
+            JOIN programme ON appartenance.num_prog = programme.num_prog
+            JOIN phase ON appartenance.num_phase = phase.num_phase
+            WHERE bonnespratique.num_bp = {arg}
+        """)
+    data = cursor.fetchall()
+    liste_bp.append(data)
 
 cursor.close()
 db.close()
 
-
-
-
-for arg in args:
-    bp_id, programme, phase = arg.split(",")
-
-
-def export_to_pdf(data):
-    
+def export_to_pdf(liste_bp):
     pdf = fpdf.FPDF()
 
-    
     pdf.add_page()
 
-    
     pdf.set_font("Arial", size=12)
 
-    
     pdf.cell(0, 10, "Bonnes pratiques", 1, 1, "C")
     pdf.ln(10)
 
-    
     pdf.cell(30, 10, "ID", 1, 0, "C")
     pdf.cell(50, 10, "Nom de la bonne pratique", 1, 0, "C")
     pdf.cell(50, 10, "Programme", 1, 0, "C")
     pdf.cell(50, 10, "Phase", 1, 1, "C")
     pdf.ln(10)
 
-    
-    for row in data:
-        pdf.cell(30, 10, str(row[0]), 1, 0, "C")
-        pdf.cell(50, 10, row[1], 1, 0, "C")
-        pdf.cell(50, 10, row[2], 1, 0, "C")
-        pdf.cell(50, 10, row[3], 1, 1, "C")
+    for bp in liste_bp:
+        pdf.cell(30, 10, str(bp['num_bp']), 1, 0, "C")
+        pdf.cell(50, 10, bp['test_bp'], 1, 0, "C")
+        pdf.cell(50, 10, bp['nom_prog'], 1, 0, "C")
+        pdf.cell(50, 10, bp['nom_phase'], 1, 1, "C")
         pdf.ln(10)
 
-    
     pdf.output("bonnes_pratiques.pdf", "F")
 
     print("PDF généré avec succès !")
-
-
-export_to_pdf(data)

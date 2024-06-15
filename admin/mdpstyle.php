17b2232380cd13d063976381a74182f2619830ee
Djigo Mamoudou <mamoudou.djigo@etu.unice.fr>
2024-06-15 20:03:26 +0200

    ajout commentaire + modif variable

--- admin/mdpstyle.php
+++ admin/mdpstyle.php
@@ -5,15 +5,15 @@
     try {
         $conn = createConnection();          // Creates a connection to the database
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            $caractere = $_POST['caractere'];
-            $chiffre = $_POST['chiffre'];
-            $majuscule = $_POST['majuscule'];
+            $character = $_POST['caractere'];
+            $number = $_POST['chiffre'];
+            $capital = $_POST['majuscule'];
             $minuscule = $_POST['minuscule'];
-            $carac = isset($_POST['carac']) ? 1 : 0;
+            $charac = isset($_POST['carac']) ? 1 : 0;
         }
         $sql = "UPDATE mdp SET caractere = ?, chiffre = ?, majuscule = ?, minuscule = ?, carac = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->execute([$caractere, $chiffre, $majuscule, $minuscule, $carac]);
+        $stmt->execute([$character, $number, $capital, $minuscule, $charac]);
         $mess ='La forme des mots de passe a été changée';
         logMessage($conn, $mess, 'MODIFICATION FORME MOT DE PASSE');
         header('Location: ../admin/admin.php');     // Redirects after successful operation         

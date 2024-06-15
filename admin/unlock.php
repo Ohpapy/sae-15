58b78b62c070c888ec87347540aeb2d4016ee6d3
Djigo Mamoudou <mamoudou.djigo@etu.unice.fr>
2024-06-15 21:16:00 +0200

    ajout commentaire + modif variable

--- admin/unlock.php
+++ admin/unlock.php
@@ -3,9 +3,9 @@
     include('../outils/log.php');
     try {
         $conn = createConnection();  
-        $sqlUnlock = "UPDATE utilisateur SET bloque_ut = 0, tentative_ut = 0 WHERE login_ut = ?";
-        $stmtUnlock = $conn->prepare($sqlUnlock);
-        $stmtUnlock->execute([$_POST['login_ut']]);
+        $sqlunlock = "UPDATE utilisateur SET bloque_ut = 0, tentative_ut = 0 WHERE login_ut = ?";
+        $stmtunlock = $conn->prepare($sqlunlock);
+        $stmtunlock->execute([$_POST['login_ut']]);
         $mess ='Un utilisateur a été débloqué avec ce login: ' . $_POST['login_ut'];
         logMessage($conn, $mess, 'UTILISATEUR DÉBLOQUÉ');
         header('Location: ../admin/admin.php');  
